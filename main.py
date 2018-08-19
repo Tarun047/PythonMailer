2c02cbc2199f3a4ab4cfff12361acb5065611e81
Tarun047 <32017154+Tarun047@users.noreply.github.com>
2018-08-19 23:17:19 +0530

    Updated GUI

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 #Import Section
 from smtplib import SMTP
 from email.mime.text import MIMEText
+from tkinter import *
+from tkinter import messagebox
 
 #Global Variables
 
 #Replace Send list with desired mail ids strings sepereated by commas
-sendList = ["some_mail_id1@some.com","some_mail_id2@some.com",]
+
 message = """
-SOME RANDOM MESSAGE
+Thank you for showing interest to attend the Python workshop.
+
+
+Please join the whatsapp group for further updates.
+https://chat.whatsapp.com/1kI7k656Y9P4fQocYwQrir
+
+
+Ignore if already joined
 """
+
+class Application:
+    def __init__(self,master=None):
+        self.master = master
+        self.Frame = Frame(self.master)
+    def main(self):
+        self.MF = Frame(self.master)
+        self.MF.pack()
+        self.WLBL = Label(self.MF,text="Enter mail ids: ")
+        self.WLBL.grid(row=0,column=0,padx=8,pady=8)
+        self.IP = Text(self.MF,height=20,width=50)
+        self.TextScroll = Scrollbar(self.IP,orient=VERTICAL)
+        self.IP.config(yscrollcommand=self.TextScroll.set)
+        self.TextScroll.config(command=self.IP.yview)
+        self.IP.grid(row=1,column=0,padx=8,pady=8)
+        self.sendBtn = Button(self.MF,text = "Send Mails",command=self.startSend)
+        self.sendBtn.grid(row=2,column=0,padx=8,pady=8)
+    def startSend(self):
+        self.mails = self.IP.get('1.0',END)
+        sendList = [mail for mail in self.mails.split('\n') if len(mail)>=5]
+        main(sendList = sendList)
+        messagebox.showinfo("Success","All mails have been sent successfully")
 
 # Function to get the message to email format
 def frame_message(recv):
@@ -19,7 +50,8 @@ def frame_message(recv):
   return m_raw
 
 # Main Function - Program Starts Here
-def main(start=0):
+def main(start=0,*,sendList):
+    print("Started Sending")
     #Connecting to your mail provider
     server = SMTP('smtp.gmail.com',587) #This is for gmail
     server.starttls() # Encrypt Message
@@ -35,7 +67,11 @@ def main(start=0):
     except Exception as e: # Incase of Server Timed out
         print(e)
         print("Error Occoured at "+ str(i)+"\n Retrying ... ")
-        main(start=i)
+        main(start=i,sendList=sendList)
     return
 
-main()
+root = Tk()
+root.title("WS Mailer")
+app = Application(master=root)
+app.main()
+root.mainloop()

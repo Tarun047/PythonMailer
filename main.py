d053c8a54d699c351c0a5cdfa781f9d9fa08a26c
Tarun047 <32017154+Tarun047@users.noreply.github.com>
2018-09-13 07:57:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,21 +4,6 @@ from email.mime.text import MIMEText
 from tkinter import *
 from tkinter import messagebox
 
-#Global Variables
-
-#Replace Send list with desired mail ids strings sepereated by commas
-
-message = """
-Thank you for showing interest to attend the Python workshop.
-
-
-Please join the whatsapp group for further updates.
-https://chat.whatsapp.com/1kI7k656Y9P4fQocYwQrir
-
-
-Ignore if already joined
-"""
-
 class Application:
     def __init__(self,master=None):
         self.master = master
@@ -26,48 +11,74 @@ class Application:
     def main(self):
         self.MF = Frame(self.master)
         self.MF.pack()
-        self.WLBL = Label(self.MF,text="Enter mail ids: ")
-        self.WLBL.grid(row=0,column=0,padx=8,pady=8)
+        self.mailLBL = Label(self.MF,text="Enter Sender Mail id ")
+        self.mailLBL.grid(row = 0,column=0,padx=8,pady=8)
+        self.mailEntry = Entry(self.MF)
+        self.mailEntry.grid(row = 0,column=1,padx=8,pady=8)
+        self.psswdLBL = Label(self.MF,text="Enter Sender Mail Password ")
+        self.psswdLBL.grid(row = 1,column=0,padx=8,pady=8)
+        self.psswdEntry = Entry(self.MF,show='*')
+        self.psswdEntry.grid(row = 1,column=1,padx=8,pady=8)
+        self.SubLBL = Label(self.MF,text = "Enter your subject here ")
+        self.SubLBL.grid(row=2,column=0,padx=8,pady=8)
+        self.SubEntry = Entry(self.MF)
+        self.SubEntry.grid(row=2,column=1,padx=8,pady=8)
+        self.MessLBL  = Label(self.MF,text = "Enter your message here ")
+        self.MessLBL.grid(row = 3,column=0,padx=8,pady=8)
+        self.MSG = Text(self.MF,height = 20,width = 50)
+        self.MSGScroll = Scrollbar(self.MSG,orient=VERTICAL)
+        self.MSG.config(yscrollcommand=self.MSGScroll.set)
+        self.MSGScroll.config(command=self.MSG.yview)
+        self.MSG.grid(row = 4,column=0,padx=8,pady=8)
+        self.WLBL = Label(self.MF,text="Enter mail ids in below Text Area ")
+        self.WLBL.grid(row=3,column=1,padx=8,pady=8)
         self.IP = Text(self.MF,height=20,width=50)
         self.TextScroll = Scrollbar(self.IP,orient=VERTICAL)
         self.IP.config(yscrollcommand=self.TextScroll.set)
         self.TextScroll.config(command=self.IP.yview)
-        self.IP.grid(row=1,column=0,padx=8,pady=8)
+        self.IP.grid(row=4,column=1,padx=8,pady=8)
         self.sendBtn = Button(self.MF,text = "Send Mails",command=self.startSend)
-        self.sendBtn.grid(row=2,column=0,padx=8,pady=8)
+        self.sendBtn.grid(row=5,columnspan=2,padx=8,pady=8)
     def startSend(self):
         self.mails = self.IP.get('1.0',END)
         sendList = [mail for mail in self.mails.split('\n') if len(mail)>=5]
-        main(sendList = sendList)
+        data = {
+        'send_id':self.mailEntry.get(),
+        'send_psswd':self.psswdEntry.get(),
+        'sub':self.SubEntry.get(),
+        'msg':self.MSG.get('1.0',END),
+        'mails':sendList,
+        }
+        main(sendData=data)
         messagebox.showinfo("Success","All mails have been sent successfully")
 
 # Function to get the message to email format
-def frame_message(recv):
+def frame_message(sender,reciever,subject,message):
   m_raw = MIMEText(message)
-  m_raw['From']="your_mail_id" #Replace with your maid id
-  m_raw['To']=recv
-  m_raw['Subject']="Invite to Python Workshop Whatsapp Group"
+  m_raw['From']=sender #Replace with your maid id
+  m_raw['To']=reciever
+  m_raw['Subject']=subject
   return m_raw
 
 # Main Function - Program Starts Here
-def main(start=0,*,sendList):
+def main(start=0,*,sendData):
     print("Started Sending")
     #Connecting to your mail provider
     server = SMTP('smtp.gmail.com',587) #This is for gmail
     server.starttls() # Encrypt Message
-    server.login('your_mail_id','your_password') # Replace with your credintials
+    server.login(sendData['send_id'],sendData['send_psswd']) # Replace with your credintials
     try:
         i = start
-        for mail in sendList[start:]:
+        for mail in sendData['mails'][start:]:
             print(mail)
-            m = frame_message(mail)
+            m = frame_message(sendData['send_id'],mail,sendData['sub'],sendData['msg'])
             # Single Line Code to send mails
-            server.sendmail("your_mail_id",mail,m.as_string())# Replace with your mail id
+            server.sendmail(sendData['send_id'],mail,m.as_string())# Replace with your mail id
             i+=1
     except Exception as e: # Incase of Server Timed out
         print(e)
         print("Error Occoured at "+ str(i)+"\n Retrying ... ")
-        main(start=i,sendList=sendList)
+        main(start=i,sendData=sendData)
     return
 
 root = Tk()
